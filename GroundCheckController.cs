967d4b8c200e52b67a9689d8192baf68404b1ce1
ramaya947 <42780472+ramaya947@users.noreply.github.com>
2020-12-17 14:14:59 -0500

    Add files via upload

---
+++ GroundCheckController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckController : MonoBehaviour
+{
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        string colLayer = LayerMask.LayerToName(c.gameObject.layer);
+        if (colLayer.CompareTo("Ground") == 0 && !isGrounded)
+            isGrounded = true;
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        string colLayer = LayerMask.LayerToName(c.gameObject.layer);
+        if (colLayer.CompareTo("Ground") == 0)
+        {
+            if (isGrounded)
+            {
+                isGrounded = false;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        string colLayer = LayerMask.LayerToName(c.gameObject.layer);
+        if (colLayer.CompareTo("Ground") == 0 && !isGrounded)
+            isGrounded = true;
+    }
+
+    public bool getIsGrounded()
+    {
+        return isGrounded;
+    }
+
+    public void setIsGrounded(bool value)
+    {
+        isGrounded = value;
+    }
+}
---
+++ MovementController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float speed = 15f, jumpHeight = 2f, fallMultiplier = 2.5f;
+    public Camera camera;
+    public GameObject groundCheck;
+
+    private Vector3 inputs;
+    private Rigidbody rb;
+
+    // Start is called before the first frame updat
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputs = Vector3.zero;
+        inputs.x = Input.GetAxisRaw("Horizontal");
+        inputs.z = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump();
+    }
+
+    private void jump()
+    {
+        if (groundCheck.GetComponent<GroundCheckController>().getIsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            groundCheck.GetComponent<GroundCheckController>().setIsGrounded(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (inputs != Vector3.zero)
+            move();
+
+        if (!groundCheck.GetComponent<GroundCheckController>().getIsGrounded())
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+    private void move()
+    {
+        //Movement
+        var cf = camera.transform.forward;
+        cf.y = 0f;
+        cf.Normalize();
+
+        var cr = camera.transform.right;
+        cr.y = 0f;
+        cr.Normalize();
+
+        var move = inputs.x * cr + inputs.z * cf;
+        transform.position += move * speed * Time.fixedDeltaTime;
+        //Movement
+
+        //Rotation
+        transform.LookAt(transform.position + move);
+        //Rotation
+    }
+}
